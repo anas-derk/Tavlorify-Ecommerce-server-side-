ec16deb32fac92fc7341f2b7a2699d2eb3dd5c78
anas derk <anas.derk2020@gmail.com>
2023-04-26 16:12:50 -0700

    Programme Get Generated Images Using Ready API

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const app = express();
 
 /* Start Config The Server */
 
-const   cors = require("cors"),
-        bodyParser = require("body-parser");
+const cors = require("cors"),
+    bodyParser = require("body-parser");
 
 app.use(cors());
 
@@ -30,5 +30,28 @@ app.listen(PORT, () => console.log(`The Server Is Running On: http://localhost:$
 const usersRouter = require("./routes/users.router");
 
 app.use("/api/users", usersRouter);
+
+app.get("/api/text-to-image-generate", async (req, res) => {
+    let textPrompt = req.query.textPrompt;
+    const Replicate = require("replicate");
+    const replicate = new Replicate({
+        auth: "r8_2uaxmAUNJTyYQQM2Wsgtvj4dSZatlLv1ssZKk"
+    });
+    try{
+        const output = await replicate.run(
+            "stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf",
+            {
+                input: {
+                    prompt: textPrompt,
+                    num_outputs: 4,
+                }
+            }
+        );
+        res.json(output);
+    }
+    catch(err) {
+        res.json(err);
+    }
+});
 
 /* End Handle The Routes */
