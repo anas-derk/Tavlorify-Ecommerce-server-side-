7a130f3fab3bad390d3fb10e547aadc4a271a48e
anas derk <anas.derk2020@gmail.com>
2023-05-02 05:41:36 -0700

    Pause Replicate API Temporarily

--- server.js
+++ server.js
@@ -33,25 +33,27 @@ app.use("/api/users", usersRouter);
 
 app.get("/api/text-to-image-generate", async (req, res) => {
     let textPrompt = req.query.textPrompt;
-    const Replicate = require("replicate");
-    const replicate = new Replicate({
-        auth: "r8_2uaxmAUNJTyYQQM2Wsgtvj4dSZatlLv1ssZKk"
-    });
-    try{
-        const output = await replicate.run(
-            "stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf",
-            {
-                input: {
-                    prompt: textPrompt,
-                    num_outputs: 4,
-                }
-            }
-        );
-        res.json(output);
-    }
-    catch(err) {
-        res.json(err);
-    }
+    console.log(textPrompt);
+    res.json(textPrompt);
+    // const Replicate = require("replicate");
+    // const replicate = new Replicate({
+    //     auth: "r8_GTYggaYuKZbUW93oFgD6Blu1JzGOGDx2NJi9J"
+    // });
+    // try{
+    //     const output = await replicate.run(
+    //         "stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf",
+    //         {
+    //             input: {
+    //                 prompt: textPrompt,
+    //                 num_outputs: 3,
+    //             }
+    //         }
+    //     );
+    //     res.json(output);
+    // }
+    // catch(err) {
+    //     res.json(err);
+    // }
 });
 
 /* End Handle The Routes */
