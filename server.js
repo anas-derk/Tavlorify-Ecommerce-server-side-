90dbe2af4d3a2fc08e43d850e3fbe5b356cbb35a
anas derk <anas.derk2020@gmail.com>
2023-05-12 05:50:25 -0700

    Handle Some Of  Text To Image Convert Cases

--- server.js
+++ server.js
@@ -29,31 +29,30 @@ app.listen(PORT, () => console.log(`The Server Is Running On: http://localhost:$
 
 const usersRouter = require("./routes/users.router");
 
-app.use("/api/users", usersRouter);
+app.use("/users", usersRouter);
 
-app.get("/api/text-to-image-generate", async (req, res) => {
-    let textPrompt = req.query.textPrompt;
-    console.log(textPrompt);
-    res.json(textPrompt);
-    // const Replicate = require("replicate");
-    // const replicate = new Replicate({
-    //     auth: "r8_GTYggaYuKZbUW93oFgD6Blu1JzGOGDx2NJi9J"
-    // });
-    // try{
-    //     const output = await replicate.run(
-    //         "stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf",
-    //         {
-    //             input: {
-    //                 prompt: textPrompt,
-    //                 num_outputs: 3,
-    //             }
-    //         }
-    //     );
-    //     res.json(output);
-    // }
-    // catch(err) {
-    //     res.json(err);
-    // }
+app.get("/text-to-image-generate", async (req, res) => {
+    let textPrompt = req.query.textPrompt,
+        prompt = req.query.prompt,
+        category = req.query.category;
+    const Replicate = require("replicate");
+    const replicate = new Replicate({
+        auth: "7a014e73594d8488f4b97e97cc083047b77ab68d"
+    });
+    try{
+        const output = await replicate.run(
+            "ai-forever/kandinsky-2:601eea49d49003e6ea75a11527209c4f510a93e2112c969d548fbb45b9c4f19f",
+            {
+                input: {
+                    prompt: `${textPrompt}, ${category}, ${prompt}`,
+                }
+            }
+        );
+        res.json(output);
+    }
+    catch(err) {
+        res.json(err);
+    }
 });
 
 /* End Handle The Routes */
