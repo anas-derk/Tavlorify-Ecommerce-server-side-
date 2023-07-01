779c3d7e97b3338407287256d0726653f268b5ca
anas derk <anas.derk2020@gmail.com>
2023-07-01 06:15:09 -0700

    Programming Download Image From Url

--- server.js
+++ server.js
@@ -37,13 +37,13 @@ app.listen(PORT, () => console.log(`The Server Is Running On: http://localhost:$
 
 /* Start Handle The Routes */
 
-const   usersRouter = require("./routes/users.router"),
-        textToImageGenerateRouter = require("./routes/textToImageGenerate.router"),
-        adminRouter = require("./routes/admin.router"),
-        categoriesRouter = require("./routes/categories.router"),
-        stylesRouter = require("./routes/styles.router"),
-        productsRouter = require("./routes/products.router"),
-        ordersRouter = require("./routes/orders.router");
+const usersRouter = require("./routes/users.router"),
+    textToImageGenerateRouter = require("./routes/textToImageGenerate.router"),
+    adminRouter = require("./routes/admin.router"),
+    categoriesRouter = require("./routes/categories.router"),
+    stylesRouter = require("./routes/styles.router"),
+    productsRouter = require("./routes/products.router"),
+    ordersRouter = require("./routes/orders.router");
 
 app.use("/users", usersRouter);
 
@@ -58,5 +58,22 @@ app.use("/styles", stylesRouter);
 app.use("/products", productsRouter);
 
 app.use("/orders", ordersRouter);
+
+app.post("/download-created-image", (req, res) => {
+    const { get } = require('axios');
+    const { createWriteStream } = require('fs');
+    const imageData = req.body;
+    const randomImageName = `${Math.random()}_${Date.now()}__${imageData.imageName}`;
+    const destination = path.join(__dirname, "assets", "images" , "generatedImages", randomImageName);
+    get(imageData.imageUrl, { responseType: 'stream' })
+        .then(response => {
+            response.data.pipe(createWriteStream(destination));
+            res.json({ msg: "success file downloaded !!", imageUrl: `assets/images/generatedImages/${randomImageName}`});
+        })
+        .catch(error => {
+            console.error('حدث خطأ أثناء تحميل الصورة:', error);
+            res.status(500).json(error);
+        });
+});
 
 /* End Handle The Routes */
