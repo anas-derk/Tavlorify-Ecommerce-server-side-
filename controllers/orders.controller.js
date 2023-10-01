0d9e945c4c13a1af54842cd76dd38ec7af409c30
anas derk <anas.derk2020@gmail.com>
2023-10-01 13:11:39 -0700

    Alter " Update Klarna Order Route Name" And Delete Routes

--- controllers/orders.controller.js
+++ controllers/orders.controller.js
@@ -6,18 +6,6 @@ async function getAllOrders(req, res) {
         .catch((err) => res.json(err));
 }
 
-async function getAllOrdersForUser(req, res) {
-    const userId = req.params.userId;
-    if (!userId) res.status(500).json("Sorry, Please Send User Id !!");
-    else {
-        const { getAllOrdersForUser } = require("../models/orders.model");
-        getAllOrdersForUser().then((result) => {
-            res.json(result);
-        })
-            .catch((err) => res.json(err));
-    }
-}
-
 async function postNewOrderToGelato(req, res) {
     const GELATO_BASE_API_URL = "https://order.gelatoapis.com";
     const GELATO_API_KEY = "4ed3eafd-b375-4c9c-bcb9-e998af3f7444-45075504-d3a7-4605-9ebb-a63a590096f1:ab1d5850-26a0-4d76-8738-bbefcc109f8f";
@@ -72,7 +60,8 @@ async function getOrderDetailsFromKlarna(req, res) {
             res.json(result);
         }
         catch(err) {
-            res.status(500).json(err);
+            console.log(err);
+            // res.status(500).json(err.response.data);
         }
     }
 }
@@ -101,7 +90,6 @@ async function putOrder(req, res) {
 
 module.exports = {
     getAllOrders,
-    getAllOrdersForUser,
     getOrderDetailsFromKlarna,
     postNewOrderToGelato,
     postNewOrderToKlarna,
--- routes/orders.router.js
+++ routes/orders.router.js
@@ -4,14 +4,12 @@ const ordersController = require("../controllers/orders.controller");
 
 ordersRouter.get("/all-orders", ordersController.getAllOrders);
 
-ordersRouter.get("/all-user-orders", ordersController.getAllOrdersForUser);
-
 ordersRouter.get("/order-details-from-klarna/:orderId", ordersController.getOrderDetailsFromKlarna);
 
 ordersRouter.post("/send-order-to-gelato", ordersController.postNewOrderToGelato);
 
 ordersRouter.post("/send-order-to-klarna", ordersController.postNewOrderToKlarna);
 
-ordersRouter.put("/update-order/:orderId", ordersController.putOrder);
+ordersRouter.put("/update-klarna-order/:orderId", ordersController.putOrder);
 
 module.exports = ordersRouter;
