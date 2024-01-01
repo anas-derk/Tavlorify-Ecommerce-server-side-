4ef3683954fd950f44b28f375b549a4600231828
anas derk <anas.derk2020@gmail.com>
2024-01-01 06:32:17 -0800

    Continue With Add Validations For Fields

--- controllers/generatedImages.controller.js
+++ controllers/generatedImages.controller.js
@@ -1,9 +1,29 @@
 async function postNewGeneratedImageData(req, res) {
     try{
         const generatedImageData = req.body;
-        const { saveNewGeneratedImageData } = require("../models/generatedImages.model");
+        const { checkIsExistValueForFieldsAndDataTypes } = require("../global/functions");
+        const checkResult = checkIsExistValueForFieldsAndDataTypes([
+            { fieldName: "Service Name", fieldValue: generatedImageData.service, dataType: "string", isRequiredValue: true },
+            { fieldName: "Upload Image URL", fieldValue: generatedImageData.uploadedImageURL, dataType: "string", isRequiredValue: generatedImageData.service === "image-to-image" },
+            { fieldName: "Text Prompt", fieldValue: generatedImageData.textPrompt, dataType: "string", isRequiredValue: generatedImageData.service === "text-to-image" },
+            { fieldName: "Category Name", fieldValue: generatedImageData.categoryName, dataType: "string", isRequiredValue: true },
+            { fieldName: "Style Name", fieldValue: generatedImageData.styleName, dataType: "string", isRequiredValue: true },
+            { fieldName: "Painting Type", fieldValue: generatedImageData.paintingType, dataType: "string", isRequiredValue: true },
+            { fieldName: "Position", fieldValue: generatedImageData.position, dataType: "string", isRequiredValue: true },
+            { fieldName: "Size", fieldValue: generatedImageData.size, dataType: "string", isRequiredValue: true },
+            { fieldName: "Is Exist White Border", fieldValue: generatedImageData.isExistWhiteBorder, dataType: "string", isRequiredValue: false },
+            { fieldName: "Width", fieldValue: Number(generatedImageData.width), dataType: "number", isRequiredValue: true },
+            { fieldName: "Height", fieldValue: Number(generatedImageData.height), dataType: "number", isRequiredValue: true },
+            { fieldName: "Frame Color", fieldValue: generatedImageData.frameColor, dataType: "string", isRequiredValue: false },
+        ]);
+        if (checkResult) {
+            await res.status(400).json(checkResult);
+            return;
+        }
+        const { saveNewGeneratedImage } = require("../global/functions");
         const result = await saveNewGeneratedImage(generatedImageData.generatedImageURL);
         if (result.msg && result.msg === "success file downloaded !!") {
+            const { saveNewGeneratedImageData } = require("../models/generatedImages.model");
             const result1 = await saveNewGeneratedImageData({
                 ...generatedImageData,
                 generatedImageURL: result.imagePath,
@@ -19,6 +39,17 @@ async function postNewGeneratedImageData(req, res) {
 async function postImageAfterCroping(req, res) {
     try {
         const cropingDetails = req.body;
+        const { checkIsExistValueForFieldsAndDataTypes } = require("../global/functions");
+        const checkResult = checkIsExistValueForFieldsAndDataTypes([
+            { fieldName: "Width", fieldValue: Number(cropingDetails.width), dataType: "number", isRequiredValue: true },
+            { fieldName: "Height", fieldValue: Number(cropingDetails.height), dataType: "number", isRequiredValue: true },
+            { fieldName: "Top", fieldValue: Number(cropingDetails.top), dataType: "number", isRequiredValue: true },
+            { fieldName: "Left", fieldValue: Number(cropingDetails.left), dataType: "number", isRequiredValue: true },
+        ]);
+        if (checkResult) {
+            await res.status(400).json(checkResult);
+            return;
+        }
         const sharp = require("sharp");
         const imagePath = `assets/images/cropedImages/cropedImage${Math.random()}_${Date.now()}__.png`;
         const imageBuffer = sharp(cropingDetails.imagePath);
@@ -42,13 +73,21 @@ async function postImageAfterCroping(req, res) {
 async function getSpecificGeneratedImagesData(req, res) {
     try{
         const service = req.query.service;
-        if (!service && (service !== "text-to-image" || service !== "image-to-image")) {
-            await res.status(400).json(`Invalid Service Name !!`);
-        } else {
-            const { getSpecificGeneratedImagesData } = require("../models/generatedImages.model");
-            const result = await getSpecificGeneratedImagesData(service);
-            await res.json(result);
+        const { checkIsExistValueForFieldsAndDataTypes } = require("../global/functions");
+        const checkResult = checkIsExistValueForFieldsAndDataTypes([
+            { fieldName: "Service Name", fieldValue: service, dataType: "string", isRequiredValue: true },
+        ]);
+        if (checkResult) {
+            await res.status(400).json(checkResult);
+            return;
         }
+        if (service !== "text-to-image" || service !== "image-to-image") {
+            await res.status(400).json(`Invalid Service Name !!`);
+            return;
+        }
+        const { getSpecificGeneratedImagesData } = require("../models/generatedImages.model");
+        const result = await getSpecificGeneratedImagesData(service);
+        await res.json(result);
     }
     catch(err){
         await res.status(500).json(err);
@@ -56,17 +95,22 @@ async function getSpecificGeneratedImagesData(req, res) {
 }
 
 async function deleteGeneratedImageData(req, res) {
-    const generatedImageDataId = req.params.generatedImageDataId;
-    if (!generatedImageDataId) res.status(400).json("Please Send Generated Image Data Id !!");
-    else {
+    try{
+        const generatedImageDataId = req.params.generatedImageDataId;
+        const { checkIsExistValueForFieldsAndDataTypes } = require("../global/functions");
+        const checkResult = checkIsExistValueForFieldsAndDataTypes([
+            { fieldName: "Generated Image Data Id", fieldValue: generatedImageDataId, dataType: "string", isRequiredValue: true },
+        ]);
+        if (checkResult) {
+            await res.status(400).json(checkResult);
+            return;
+        }
         const { deleteGeneratedImageData } = require("../models/generatedImages.model");
-        try{
-            const result = await deleteGeneratedImageData(generatedImageDataId);
-            await res.json(result);
-        }
-        catch(err) {
-            await res.status(500).json(err);
-        }
+        const result = await deleteGeneratedImageData(generatedImageDataId);
+        await res.json(result);
+    }
+    catch(err) {
+        await res.status(500).json(err);
     }
 }
 
--- controllers/prices.controller.js
+++ controllers/prices.controller.js
@@ -1,18 +1,26 @@
 async function getPricesByProductName(req, res) {
     try{
         const productName = req.query.productName;
-        if (!productName) await res.status(400).json("Please Send Product Name !!");
-        else if (
+        const { checkIsExistValueForFieldsAndDataTypes } = require("../global/functions");
+        const checkResult = checkIsExistValueForFieldsAndDataTypes([
+            { fieldName: "Product Name", fieldValue: productName, dataType: "string", isRequiredValue: true },
+        ]);
+        if (checkResult) {
+            await res.status(400).json(checkResult);
+            return;
+        }
+        if (
             productName !== "poster" &&
             productName !== "wooden-frame" &&
             productName !== "hanger" &&
             productName !== "canvas"
-        ) await res.status(400).json("Please Send Valid Product Name !!");
-        else {
-            const { getPricesByProductName } = require("../models/prices.model");
-            const result = await getPricesByProductName(productName);
-            await res.json(result);
+        ) {
+            await res.status(400).json("Please Send Valid Product Name !!");
+            return;
         }
+        const { getPricesByProductName } = require("../models/prices.model");
+        const result = await getPricesByProductName(productName);
+        await res.json(result);
     }
     catch(err) {
         await res.status(500).json(err);
@@ -24,12 +32,19 @@ async function putProductPrice(req, res) {
         const productId = req.params.productId;
         const   newProductPriceBeforeDiscount = req.body.newProductPriceBeforeDiscount,
                 newProductPriceAfterDiscount = req.body.newProductPriceAfterDiscount;
-        if (!productId) await res.status(400).json("Please Send Product Id !!");
-        else {
-            const { updateProductPrice } = require("../models/prices.model");
-            const result = await updateProductPrice(productId, newProductPriceBeforeDiscount, newProductPriceAfterDiscount);
-            await res.json(result);
+        const { checkIsExistValueForFieldsAndDataTypes } = require("../global/functions");
+        const checkResult = checkIsExistValueForFieldsAndDataTypes([
+            { fieldName: "Product Id", fieldValue: productId, dataType: "string", isRequiredValue: true },
+            { fieldName: "New Product Price Before Discount", fieldValue: Number(newProductPriceBeforeDiscount), dataType: "number", isRequiredValue: true },
+            { fieldName: "New Product Price After Discount", fieldValue: Number(newProductPriceAfterDiscount), dataType: "number", isRequiredValue: true },
+        ]);
+        if (checkResult) {
+            await res.status(400).json(checkResult);
+            return;
         }
+        const { updateProductPrice } = require("../models/prices.model");
+        const result = await updateProductPrice(productId, newProductPriceBeforeDiscount, newProductPriceAfterDiscount);
+        await res.json(result);
     }
     catch(err) {
         await res.status(500).json(err);
@@ -41,12 +56,19 @@ async function getPriceByProductDetails(req, res) {
         const   productName = req.query.productName,
                 position = req.query.position,
                 dimentions = req.query.dimentions;
-        if (!productName || !dimentions || !position) await res.status(400).json("Please Send All Product Details !!");
-        else {
-            const { getPriceByProductDetails } = require("../models/prices.model");
-            const result = await getPriceByProductDetails(productName, position, dimentions);
-            await res.json(result);
+        const { checkIsExistValueForFieldsAndDataTypes } = require("../global/functions");
+        const checkResult = checkIsExistValueForFieldsAndDataTypes([
+            { fieldName: "Product Name", fieldValue: productName, dataType: "string", isRequiredValue: true },
+            { fieldName: "Position", fieldValue: position, dataType: "string", isRequiredValue: true },
+            { fieldName: "Dimentions", fieldValue: dimentions, dataType: "string", isRequiredValue: true },
+        ]);
+        if (checkResult) {
+            await res.status(400).json(checkResult);
+            return;
         }
+        const { getPriceByProductDetails } = require("../models/prices.model");
+        const result = await getPriceByProductDetails(productName, position, dimentions);
+        await res.json(result);
     }
     catch(err) {
         await res.status(500).json(err);
