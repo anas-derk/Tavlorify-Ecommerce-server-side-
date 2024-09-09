0e90eebaf2a899112a1d2bf976c757acb6d3d472
anas derk <anas.derk2020@gmail.com>
2024-09-10 01:38:46 +0300

    Fix

--- controllers/generatedImages.controller.js
+++ controllers/generatedImages.controller.js
@@ -45,7 +45,7 @@ async function generateImageUsingTextToImageService(req, res) {
     try{
         const textAfterTranslation = await translateText(textPrompt);
         let tempOutput;
-        switch (model_name) {
+        switch (result.data.modelName) {
             case "dreamshaper": {
                 const output = await runModel("cjwbw/dreamshaper:ed6d8bee9a278b0d7125872bddfb9dd3fc4c401426ad634d8246a660e387475b",
                 {
