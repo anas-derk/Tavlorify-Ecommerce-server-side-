c78ac9771f471289ea4e25554366e1e4e5ab8b38
anas derk <anas.derk2020@gmail.com>
2024-11-05 13:37:35 +0300

    Test

--- controllers/styles.controller.js
+++ controllers/styles.controller.js
@@ -60,6 +60,7 @@ async function putStyleImage(req, res) {
         res.json(result);
     }
     catch(err) {
+        console.log(err)
         res.status(500).json(getResponseObject("Internal Server Error !!", true, {}));
     }
 }
